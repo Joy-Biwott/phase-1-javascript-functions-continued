34bc2c7adcea18e206b8b790808de8944d130ee5
Joy <biwott.joy.03@gmail.com>
2022-08-17 18:26:49 +0300

    Completed Assighnment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (string = 'roller-skate'){
+    return `This Saturday, I want to ${string}!`;
+}
+
+
+function mondayWork (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective (adj = "*"){
+    return function(param = "special"){
+        return `You are ${adj}${param}${adj}!`;
+    }
+    const encouragingPromptFunction = wrapAdjective("!!!")
+}
+
